6658e1da915ba243c5691573d04c559c82369aad
Alessandro-Moroni <alemoro98@gmail.com>
2024-02-22 15:27:07 +0100

    [color]quadrato

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 const quadrato = document.getElementById('cont-quadrato');
 
 for(let i = 1; i < 100; i++){
-  quadrato.innerHTML += `<div class="box">${i}</div>`
+  const box = document.createElement('div')
+  box.className = 'box';
+  box.innerHTML = i;
+  if(!(i % 3)) box.classList.add('tre');
+  quadrato.append(box);
+  if(!(i % 5)) box.classList.add('cinque');
+  quadrato.append(box);
+  
 }
