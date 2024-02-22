1a512ee18a0bac18363d9b30295fb18b6bfdfb91
Alessandro-Moroni <alemoro98@gmail.com>
2024-02-22 16:14:58 +0100

    [finish]all+bonus

--- js/script.js
+++ js/script.js
@@ -3,10 +3,24 @@ const quadrato = document.getElementById('cont-quadrato');
 for(let i = 1; i < 100; i++){
   const box = document.createElement('div')
   box.className = 'box';
-  box.innerHTML = i;
-  if(!(i % 3)) box.classList.add('tre');
-  quadrato.append(box);
-  if(!(i % 5)) box.classList.add('cinque');
-  quadrato.append(box);
   
+  if((!(i % 3)) && (!(i % 5))) {
+    box.classList.add('tre-cinque');
+    box.innerHTML = 'fizzbuzz';
+
+  }else if(!(i % 3)) {
+    box.classList.add('tre');
+    box.innerHTML = 'fizz';
+    
+  }else if(!(i % 5)) {
+    box.classList.add('cinque');
+    box.innerHTML = 'buzz';
+
+  }else{
+    box.innerHTML = i;
+  }
+
+  quadrato.append(box);
 }
+
+
